b8df1f7c84d17c798953231fa2ce25e24c6c4792
Alana <rodt.alana@gmail.com>
2020-11-12 21:17:44 -0300

    Exercicios Java

---
+++ ProjetoJava/src/PacoteJava/Ex01.java
@@ -0,0 +1,19 @@
+package PacoteJava;
+
+public class Ex01 {
+	//Utilize os conhecimentos adquiridos nos vídeos de introdução a java para apresentar em console 
+	//um pouco mais sobre você, hobbies, o que você faz nas horas vagas o que você gosta e o que você 
+	//não gosta... 
+			public static void main (String args[])
+			{
+				System.out.println("Olá! Sou Alana, tenho 25 aninhos e tô me aventurando no mundo dos\n"
+						+ "códigos e erros. Eu gosto muito de ficar em silêncio, andar de bicicleta, \n"
+						+ "pesquisar sites divertidos, tomar suco de cambuci, ajudar, assistir filmes - \n"
+						+ "principalmente desenhos!!! -, ler, aprender, ouvir músicas instrumentais, cozinhar e \n"
+						+ "comer tapioca com carne seca e pimentinha de cheiro. Adoro ver o pôr-do-sol \n"
+						+ "e de ficar no mato. Não gosto de competir nem de estar em ambientes muito \n"
+						+ "agitados/lotados/sufocantes. \n"
+						+ "Agora, uma piada: sabe onde os astronautas fazem home office?" 
+						+ "\n R: Na sala de Star, porque eles precisam de espaço");
+			}
+}
---
+++ ProjetoJava/src/PacoteJava/Ex02.java
@@ -0,0 +1,20 @@
+package PacoteJava;
+
+public class Ex02 {
+//Utilize os conhecimentos adquiridos nos vídeos de introdução a java para apresentar em console 
+//quais foram as habilidades e mentalidades que você mais usou para chegar até aqui,
+
+		public static void main (String args[])
+		{
+			System.out.println("\n Das quatro habilidades - trabalho em equipe, comunicação, atenção aos"
+					+ "\n detalhes e gerenciamento de tempo - acredito que todas estão sendo essenciais"
+					+ "\n no processo até aqui, sobretudo as três primeiras. E, ainda, das três, trabalho"
+					+ "\n em equipe tem sido a habilidade comportamental que mais tenho desenvolvido e "
+					+ "\n aprendido, não só pela dinâmica e cultura em si do bootcamp, mas, também, nos "
+					+ "\n momentos assíncronos, quando estudamos juntos e uns ajudam os outros."
+					+ "\n Em relação às mentalidades, onde todas também têm sido presentes, destaco a persistência,"
+					+ "\n uma vez que ela tem sido de suma importância para o estudo, cumprimento de atividades e"
+					+ "\n todos os passos que têm envolvido o meu processo de aprendizagem nessas 3 semanas. ");
+		}
+
+}
